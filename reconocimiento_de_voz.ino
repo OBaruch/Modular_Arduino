d6b13e75d2421ce682a9d56c4ebc04b540650249
ArodPre <57921218+ArodPre@users.noreply.github.com>
2019-11-18 15:34:34 -0600

    Add files via upload

---
+++ reconocimiento_de_voz.ino
@@ -0,0 +1,118 @@
+//lib necessária para conectar o wifi
+#include <WiFi.h>
+//led conectado no pino 23
+#define ledVerde 23
+//mensagem enviada pelo client (aplicativo)
+String ClientRequest;
+//ip estático, o mesmo deve ser usado no app do smartphone
+IPAddress staticIP(192,168,0,25);
+//gateway, deixe aqui o gateway da rede em que está conectado
+IPAddress gateway(192,168,3,255);
+//máscara, deixe aqui a máscara da rede em que está conectado
+IPAddress subnet(255,255,255,0);
+//objeto do servidor
+WiFiServer server(80);
+//objeto do cliente
+WiFiClient client;
+//variável usada para obter o request do client
+String myresultat;
+
+//função usada para a leitura do request sem caracteres de quebra de linha como "\n" ou "\r"
+String ReadIncomingRequest()
+{
+//enquanto houver bytes enviados pelo client
+while(client.available())
+{
+//atribui para a variável String o comando enviado pelo cliente sem "\r"
+ClientRequest = (client.readStringUntil('\r'));
+//se existir "HTTP/1.1" na String então recebe comando, senão o comando não é aceito
+//isso verifica que a solicitação seja HTTP/1.1
+if ((ClientRequest.indexOf("HTTP/1.1")>0))
+myresultat = ClientRequest;
+}
+//retorna variável
+return myresultat;
+}
+
+
+
+void setup()
+{
+//inicializa varíavel como vazia
+ClientRequest = "";
+//define pino do led como saída
+pinMode(ledVerde,OUTPUT);
+//inicializa serial com 115200 bits por segundo
+Serial.begin(115200);
+//aguarda 10ms
+delay(10);
+//A partir daqui conecta wifi
+Serial.println("START");
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println("JyL_Mazamitla (Planta Alta)");
+//configura ssid e senha da rede
+WiFi.begin("JyL_Mazamitla (Planta Alta)", "L21503733711108");
+//enquanto não conectar exibe "."
+while (WiFi.status() != WL_CONNECTED)
+{
+delay(500);
+Serial.print(".");
+}
+//exibe "conectado"
+Serial.println("Connected");
+//configura ip estático, gateway e máscara (definidos globais no início do código)
+WiFi.config(staticIP, gateway, subnet);
+//exibe ip utilizado pelo ESP
+Serial.println("Your IP is");
+Serial.println((WiFi.localIP()));
+//inicializa servidor
+server.begin();
+}
+
+
+void loop()
+{
+//obtém cliente
+client = server.available();
+//se ele for nulo, retorna até que ele realmente exista
+if (!client)
+return;
+//enquanto não existir request aguarda
+while(!client.available())
+delay(1);
+//obtém request utilizando a função local ReadIncomingRequest
+ClientRequest = (ReadIncomingRequest());
+Serial.println("respuesta");
+//retira dados da página e obtém apenas o comando enviado
+ClientRequest.remove(0, 5);
+ClientRequest.remove(ClientRequest.length()-9,9);
+//controla led conforme o comando recebido
+if (ClientRequest == "prender")
+digitalWrite(ledVerde,HIGH);
+if (ClientRequest == "apagar")
+digitalWrite(ledVerde,LOW);
+if (ClientRequest == "parpadear")
+{
+digitalWrite(ledVerde,HIGH);
+delay(500);
+digitalWrite(ledVerde,LOW);
+delay(500);
+digitalWrite(ledVerde,HIGH);
+delay(500);
+digitalWrite(ledVerde,LOW);
+delay(500);
+}
+//exibe na página a palavra "OK", caso acessado por um navegador
+//se estiver no aplicativo esta exibição não será feita
+client.println("HTTP/1.1 200 OK");
+client.println("Content-Type: text/html");
+client.println("");
+client.println("");
+client.println("");
+client.println("OK");
+client.println("");
+client.flush();
+client.stop();
+delay(1);
+}
